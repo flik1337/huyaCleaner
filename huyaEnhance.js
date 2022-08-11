2391addf71b4a444eef03b63a03d404fa0d2bbab
flik <664604982@qq.com>
2022-08-11 12:28:31 +0800

    add special ad

--- huyaEnhance.js
+++ huyaEnhance.js
@@ -1,61 +1,72 @@
-// ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       flik
-// @match        https://www.huya.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
-// @grant        GM_addStyle
-// ==/UserScript==
+    // ==UserScript==
+    // @name         New Userscript
+    // @namespace    http://tampermonkey.net/
+    // @version      0.1
+    // @description  try to take over the world!
+    // @author       flik
+    // @match        https://www.huya.com/*
+    // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+    // @grant        GM_addStyle
+    // @grant        GM_setValue
+    // @grant        GM_getValue
+    // ==/UserScript==
 
 
-(function() {
-    'use strict';
-    // 顶部广告
-    let topBannerCss =   `
-    .diy-toutu ,.J_ttVideo{
-        margin: 0 auto 120px !important;
-        background-image:none !important;
-        height:20px !important;
-    }
-    `   
-    // 组件广告
-    let CompAdCss =   `
-        .diy-comps-wrap{
-            display:none;
-        }
-    `
-    // 底部广告
-    let mainAdCss =   `
-        .main-room{
+    (function() {
+        'use strict';
+        // 顶部广告
+        let topBannerCss =   `
+        .diy-toutu,
+        .J_ttVideo,
+        .special-bg{
+            margin: 0 auto 120px !important;
             background-image:none !important;
+            height:20px !important;
         }
-    `
-    //视频左下方广告
-    let videoBannerAdCss = `
-        #huya-ab-fixed{
+        `   
+        // 组件广告
+        let compAdCss =   `
+            .diy-comps-wrap{
+                display:none;
+            }
+        `
+        // 底部广告
+        let mainAdCss =   `
+            .main-room{
+                background-image:none !important;
+            }
+        `
+        //视频左下方广告
+        let videoBannerAdCss = `
+            #huya-ab-fixed,
+            #huya-ab{
+                display:none;
+            }
+        `
+        // 游戏广告牌
+        let businessGameAdCss = `
+            .room-business-game ,.room-gg-chat{
+                display:none !important;
+            }
+        `
+        // 下载app提示
+        let downloadAppAdCss = `
+            #player-download-guide-tip{
+                display:none;
+            }
+        `
+        let competitionAdCss = `
+        .competition_cont_center_wrap{
             display:none;
         }
-    `
-    // 游戏广告牌
-    let businessGameAdCss = `
-        .room-business-game ,.room-gg-chat{
-            display:none !important;
-        }
-    `
-    // 下载app提示
-    let downloadAppAdCss = `
-        #player-download-guide-tip{
-            display:none;
-        }
-    `
+        `
 
-    GM_addStyle(topBannerCss)
-    GM_addStyle(CompAdCss)
-    GM_addStyle(mainAdCss)
-    GM_addStyle(videoBannerAdCss)
-    GM_addStyle(businessGameAdCss)
-    GM_addStyle(downloadAppAdCss)
+        GM_addStyle(topBannerCss)
+        GM_addStyle(compAdCss)
+        GM_addStyle(mainAdCss)
+        GM_addStyle(videoBannerAdCss)
+        GM_addStyle(businessGameAdCss)
+        GM_addStyle(downloadAppAdCss)
+        GM_addStyle(competitionAdCss)
 
-})();
+    })();
